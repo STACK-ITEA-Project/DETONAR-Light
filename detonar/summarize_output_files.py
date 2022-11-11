16641ef350211470f59f8b0ba6d59bd8b4d94bc6
vix <victoriabull@live.se>
2022-11-11 13:22:14 +0100

    Updated summary to include statistics

--- detonar/summarize_output_files.py
+++ detonar/summarize_output_files.py
@@ -22,6 +22,7 @@ attack_names = {
     'Blackhole': 'BLACKHOLE/SEL FORWARD',
     'Selective_Forward': 'BLACKHOLE/SEL FORWARD',
     'Clone-ID': 'CLONE-ID or SYBIL',
+    'Clone_ID': 'CLONE-ID or SYBIL',
     'Clone_Id': 'CLONE-ID or SYBIL',
     'Sybil': 'CLONE-ID or SYBIL',
     'DIS': 'DIS',
@@ -34,29 +35,24 @@ attack_names = {
     'Worst_Parent': 'WORST PARENT'
 }
 
-expected_attack_dict = {'BLACKHOLE/SEL FORWARD': [], 'CLONE-ID or SYBIL': [], 'DIS': [],
-                        'HELLO FLOOD': [], 'RANKS': [],
-                        'VERSION': [], 'WORMHOLE': [], 'WORST PARENT': []}
+def get_key(dict, val):
+    for key, value in dict.items():
+        if val == value:
+            return key
 
+    return "key doesn't exist"
+def get_expected_attacks_lines(info_file_path, args):
+    expected_lines = []
+    with open(info_file_path[0], 'r') as f:
+        lines = f.readlines()
 
-def fill_expected_attacks_dict(scenario_paths, args):
-    info_paths = []
-    for scenario_path in scenario_paths:
-        info_paths.append(glob.glob(os.path.join(scenario_path, "Packet_Trace_{}s".format(args.sim_time), '*.txt')))
+        for line in lines:
+            if 'Scenario' in line:
+                attack_name = line.split('Scenario')[-1].rstrip().lstrip()
 
-    for info_path in info_paths:
-        with open(info_path[0], 'r') as f:
-            lines = f.readlines()
-
-            for line in lines:
-                if 'Scenario' in line:
-                    attack_name = line.split('Scenario')[-1].rstrip().lstrip()
-                else:
-                    sim_nr = line.split('-')[0].rstrip().lstrip()
-                    attacker = line.split('-')[1].split('attacker: ')[1].split('at')[0].rstrip().lstrip()
-                    attack_time = line.split('time: ')[-1].rstrip().lstrip()
-            if attack_name != 'Legit':
-                expected_attack_dict[attack_names[attack_name]].append({'sim_nr': sim_nr, 'attacker': attacker, 'time': attack_time})
+            else:
+                expected_lines.append(line)
+    return expected_lines
 
 
 def parse_attack_line(line, last_line, last_time):
@@ -90,7 +86,7 @@ def parse_attack_line(line, last_line, last_time):
     return attack, last_line, last_time, time, attacker_node
 
 
-def process_file(file, result_file):
+def process_file(file, result_file, expected_attacks_lines, scenario):
     correctly_classified_sims = 0
     correctly_identified_attackers = 0
     f = open(file, 'r')
@@ -100,6 +96,11 @@ def process_file(file, result_file):
     filename = file.split('/')[-1]
     # Remove filetype
     sim_number = filename.split('.')[0]
+    if scenario != 'Legit':
+        expected_result_line = [line for line in expected_attacks_lines if sim_number in line]
+        expected_attacker = expected_result_line[0].split('-')[-1].split('attacker: ')[-1].split('at')[0].rstrip().lstrip()
+        expected_attack_time = expected_result_line[0].split('time: ')[-1].rstrip().lstrip()
+
     attack_dict = {'BLACKHOLE/SEL FORWARD': [[], []], 'CLONE-ID or SYBIL': [[], []], 'DIS': [[], []],
                    'HELLO FLOOD': [[], []], 'RANKS': [[], []],
                    'VERSION': [[], []], 'WORMHOLE': [[], []], 'WORST PARENT': [[], []]}
@@ -110,17 +111,14 @@ def process_file(file, result_file):
             attack, last_line, last_time, attack_time, attacker_node = parse_attack_line(line, last_line, last_time)
             attack_dict[attack][0].append(attack_time)
             attack_dict[attack][1].append(attacker_node)
-
-            for sim in expected_attack_dict[attack]:
-                if sim['sim_nr'] == sim_number:
-                    expected_attacker = sim['attacker']
-                    expected_attack_time = sim['time']
-
-                if int(attack_time) > int(expected_attack_time) / 1000000 and not first_correct_attack:
+            if scenario != 'Legit':
+                if (expected_attacker in attacker_node) and correctly_identified_attackers == 0:
+                    correctly_identified_attackers += 1
+                if int(attack_time) > int(expected_attack_time) / 1000000 and (attack == attack_names[scenario]) and not first_correct_attack:
                     first_correct_attack = True
                     correctly_classified_sims += 1
-                    if expected_attacker in attacker_node:
-                        correctly_identified_attackers += 1
+
+
         else:
             last_time = 0
             last_line = line
@@ -138,7 +136,10 @@ def process_file(file, result_file):
             for node in attack_dict[attack][1]:
                 if node not in attackers:
                     attackers.append(node)
-            nodes_str = [attacker + ', ' for attacker in attackers][0]
+            nodes_str = ""
+            nodes_list = [attacker.strip("\'") for attacker in attackers]
+            for node in nodes_list:
+                nodes_str = nodes_str + node + ', '
             str = str + attack + '(' + times_str + ', attackers: ' + nodes_str + '), '
     f.close()
     result_file.write('{}: {}\n'.format(sim_number, str))
@@ -157,19 +158,29 @@ def main(args = Args()):
     result_file = open(output_filename, 'w')
 
     # Gather the expected result from dataset-folder
-    fill_expected_attacks_dict(scenario_paths, args)
+
+    info_paths = []
+    for scenario_path in scenario_paths:
+        info_paths.append(glob.glob(os.path.join(scenario_path, "Packet_Trace_{}s".format(args.sim_time), '*.txt'))[0])
 
     # For each scenario, process output file for each simulation
     for scenario in scenarios:
+        info_file_path = [path for path in info_paths if scenario in path]
+        expected_attacks_lines = get_expected_attacks_lines(info_file_path, args)
+
         if os.path.exists(os.path.join(os.getcwd(), 'log', args.output_dir.format(args.sim_time, args.time_window, datetime.date.today()), scenario)):
             filenames = glob.glob(os.path.join(os.getcwd(), 'log', args.output_dir.format(args.sim_time, args.time_window, datetime.date.today()), scenario, '*'))
             result_file.write('{}\n'.format(scenario))
             # Variables for printing statistics for each scenario
             total_sims = len(filenames)
             # Parse each output file
+            correctly_classified_sims_result = 0
+            correctly_identified_attackers_result = 0
             for file in filenames:
-                correctly_classified_sims, correctly_identified_attackers = process_file(file, result_file)
-            result_file.write('Results: {}/{} correctly classified, {}/{} identified attacker\n'.format(correctly_classified_sims,total_sims, correctly_identified_attackers, total_sims))
+                correctly_classified_sims, correctly_identified_attackers = process_file(file, result_file, expected_attacks_lines, scenario)
+                correctly_classified_sims_result += correctly_classified_sims
+                correctly_identified_attackers_result += correctly_identified_attackers
+            result_file.write('Results: {}/{} correctly classified, {}/{} identified attacker\n'.format(correctly_classified_sims_result,total_sims, correctly_identified_attackers_result, total_sims))
             result_file.write('\n')
 
     result_file.close()
